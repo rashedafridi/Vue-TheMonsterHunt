3ea596b5cd19ac14c0ecf06ef2bd695c4aae9e9c
Rashed Afridi <rashedafridi@gmail.com>
2021-04-24 15:04:23 +0600

    shack effect added

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+function hendelShack(element){
+    if(element === 'hero' )
+    {   console.log('hero')
+        document.getElementById('hero').classList.add('shake')
+        setTimeout(() => {
+            document.getElementById('hero').classList.remove('shake')
+            console.log('hero remove')
+        }, 100);
+    }
+    else{
+        document.getElementById('monster').classList.add('shake')
+        setTimeout(() => {
+            document.getElementById('monster').classList.remove('shake')
+        }, 100);
+    }
+}
 function rendomattuckValue(lowerLimit,UperLimit)
 {
     return Math.floor(Math.random() * (UperLimit - lowerLimit)) + lowerLimit;  
@@ -37,19 +53,26 @@ const app = Vue.createApp({
             const attackValue= rendomattuckValue(2,10)
             this.monsterHealt -= attackValue
             this.addLog('hero' ,'Attack',attackValue)
-            this.monsterAttackHero()
             this.currentRound++
+            hendelShack('monster')
+            
+            this.monsterAttackHero()
+            
+            
         },
         monsterAttackHero(){
             const attackValue= rendomattuckValue(6,15)
             this.HeroHealt -= attackValue
+            hendelShack('hero')
             this.addLog('monster' ,'Attack',attackValue)
         },
         specialAttackMonster(){
             this.currentRound++
             const attackValue= rendomattuckValue(8,20)
             this.monsterHealt -= attackValue
+            hendelShack('monster')
             this.addLog('hero' ,'specialAttack',attackValue)
+           
 
         },
         healHero() {
